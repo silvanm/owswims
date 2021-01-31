f7872632337b9ae8c532922e90b7ef7982316f0d
Silvan Muehlemann <silvan.muehlemann@muehlemann-popp.ch>
2021-01-31 21:35:10 +0100

    Allow embedding page everywhere

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
+
+@xframe_options_exempt
 def index(request):
     return render(request, template_name="index.html")
