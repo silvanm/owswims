b9246ed2e73d5bf37259061969accb22db952766
Silvan Muehlemann <silvan.muehlemann@muehlemann-popp.ch>
2021-02-01 07:44:09 +0100

    Make scrollbar always visible

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -186,6 +186,16 @@ body {
   position: fixed;
 }
 
+::-webkit-scrollbar {
+  -webkit-appearance: none;
+  width: 7px;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 4px;
+  background-color: rgba(0, 0, 0, 0.5);
+  -webkit-box-shadow: 0 0 1px rgba(255, 255, 255, 0.5);
+}
+
 a {
   @apply text-blue-600;
 }
