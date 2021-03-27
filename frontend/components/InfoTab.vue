a2b26c9fbd1967888b99fc13c0f6c4637df010fd
Silvan Muehlemann <silvan.muehlemann@muehlemann-popp.ch>
2021-03-27 21:36:20 +0100

    Add imprint

--- frontend/components/InfoTab.vue
+++ frontend/components/InfoTab.vue
@@ -10,23 +10,41 @@
       >
         {{ $t('titleContributors') }}
       </li>
+      <li
+        :class="{ active: activeTab === 'imprint' }"
+        @click="activeTab = 'imprint'"
+      >
+        {{ $t('titleImprint') }}
+      </li>
     </ul>
     <div id="help" v-if="activeTab === 'help'">
       <Infotext></Infotext>
+      <div class="inline-block cursor-pointer">
+        <a
+          v-if="$store.getters['auth/loggedIn']"
+          class="mr-2"
+          @click="$store.dispatch('auth/logout')"
+          >{{ $t('logout') }}</a
+        >
+        <span v-else>
+          <a @click="$emit('showLogin')">{{ $t('login') }}</a>
+        </span>
+      </div>
     </div>
     <div id="contributors" v-if="activeTab === 'contributors'">
       <p>{{ $t('infoboxThanks') }}</p>
     </div>
-    <div class="inline-block cursor-pointer">
-      <a
-        v-if="$store.getters['auth/loggedIn']"
-        class="mr-2"
-        @click="$store.dispatch('auth/logout')"
-        >{{ $t('logout') }}</a
-      >
-      <span v-else>
-        <a @click="$emit('showLogin')">{{ $t('login') }}</a>
-      </span>
+    <div id="imprint" v-if="activeTab === 'imprint'">
+      <p>
+        Silvan Mühlemann<br />
+        c/o Mühlemann&Popp Online Media AG<br />
+        Limmatquai 122<br />
+        8001 Zürich<br />
+        +41 78 714 14 78<br />
+        <a href="mailto:silvan@open-water-swims.com"
+          >silvan@open-water-swims.com</a
+        >
+      </p>
     </div>
   </div>
 </template>
